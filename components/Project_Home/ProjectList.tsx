86839bf0560bf66c6bb6b83a8cc2cbc84f057f28
massimiliano <massimarcello@hotmail.it>
2024-10-27 00:50:34 +0200

    before adding main project in schema

--- components/Project_Home/ProjectList.tsx
+++ components/Project_Home/ProjectList.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import ProjectInfos from "./ProjectInfo";
+import ProjectInfo from "./ProjectInfo";
 import TechnologiesUsed from "./TecnologiesUsed";
 import { PortableText } from '@portabletext/react';
 import styles from "./ProjectList.module.scss"; 
-
-
-
-
 
 interface Project {
   _id: string;
@@ -36,13 +32,19 @@ const ProjectList: React.FC<ProjectListProps> = ({ projects, openProjectId, togg
           onClick={() => toggleProjectInfo(project._id)}
         >
           <div className={styles.boxImageTextProject}>
+          <h3>{project.name}</h3>
             <img className={styles.projectImage} src={project.image} alt={project.imageAlt} />
             <div className={styles.portableStyle}>
               <PortableText value={project.content[0]} />
             </div>
           </div>
           <TechnologiesUsed technologies={project.technologies} />
-          <ProjectInfos project={project} openProjectId={openProjectId} />
+          {/* Mostra le info del progetto solo se il progetto è aperto */}
+          {openProjectId === project._id && (
+            <div className={styles.infoBubble}>
+              <ProjectInfo project={project} openProjectId={null} />
+            </div>
+          )}
         </div>
       ))}
     </div>
@@ -50,3 +52,4 @@ const ProjectList: React.FC<ProjectListProps> = ({ projects, openProjectId, togg
 };
 
 export default ProjectList;
+
