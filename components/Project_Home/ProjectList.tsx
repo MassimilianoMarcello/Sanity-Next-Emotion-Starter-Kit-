73b6916dea48365b4bac8ef2c60583c8826579f0
massimiliano <massimarcello@hotmail.it>
2024-12-04 02:48:01 +0100

    label in the left category importance

--- components/Project_Home/ProjectList.tsx
+++ components/Project_Home/ProjectList.tsx
@@ -4,14 +4,7 @@ import TechnologiesUsed from "./TecnologiesUsed";
 import { PortableText } from "@portabletext/react";
 import { Project } from "@/types/projects";
 import styles from "./ProjectList.module.scss";
-import "../../app/main.module.scss";
 import Link from "next/link";
-
-interface Technology {
-  _id: string;
-  name: string;
-  slug: string;
-}
 
 interface ProjectListProps {
   projects: Project[];
@@ -24,61 +17,71 @@ const ProjectList: React.FC<ProjectListProps> = ({
   openProjectId,
   toggleProjectInfo,
 }) => {
-  console.log("Projects received in ProjectList:", projects);
+  // Raggruppare i progetti per importanza
+  const groupedProjects = projects.reduce((acc, project) => {
+    if (!acc[project.importance]) {
+      acc[project.importance] = [];
+    }
+    acc[project.importance].push(project);
+    return acc;
+  }, {} as Record<string, Project[]>);
 
-  if (projects.length === 0) {
-    return <p>No projects available.</p>;
-  }
+  // Ordina le categorie per l'ordine desiderato
+  const importanceOrder = ["main", "secondary", "testing"];
+  const sortedCategories = importanceOrder.filter((category) =>
+    groupedProjects[category]
+  );
 
   return (
-    <div className={styles.projectContainer}>
-      {projects.map((project) => (
-        <div
-          key={project._id}
-          className={`${styles.projectCard} ${
-            project.importance === "main" ? "main" : "other-style"
-          }`}
-          onClick={() => toggleProjectInfo(project._id)}
-        >
-          <div className={styles.boxTextProject}>
-            <h3>{project.name}</h3>
-            <h3>{project.importance}</h3>
-            <h3>{project.status}</h3>
-            <h3>{project.status}</h3>
-         
-            <div className={styles.portableStyle}>
-              <PortableText value={project.content[0]} />
-            </div>
+    <div className={styles.projectListContainer}>
+      {sortedCategories.map((category) => (
+        <div key={category} className={styles.categorySection}>
+          <div className={styles.categoryLabel}>{category}</div>
+          <div className={styles.projectCards}>
+            {groupedProjects[category].map((project) => (
+              <div
+                key={project._id}
+                className={styles.projectCard}
+                onClick={() => toggleProjectInfo(project._id)}
+              >
+                <div className={styles.boxTextProject}>
+                  <h3>{project.name}</h3>
+                  <div className={styles.portableStyle}>
+                    <PortableText value={project.content[0]} />
+                  </div>
+                </div>
+                <div className={styles.boxImageProject}>
+                  <img
+                    className={styles.projectImage}
+                    src={project.image}
+                    alt={project.imageAlt}
+                  />
+                </div>
+                <div className={styles.visitRepoAndWebsiteButtons}>
+                  <div className={styles.styledButton}>
+                    <Link href={project.githubUrl}>GitHub</Link>
+                  </div>
+                  <div className={styles.styledButton}>
+                    <Link href={project.url}>Visit Website</Link>
+                  </div>
+                </div>
+                <div className={styles.technologiesUsed}>
+                  <TechnologiesUsed technologies={project.technologies} />
+                </div>
+                {openProjectId === project._id && (
+                  <div className={styles.infoBubble}>
+                    <ProjectInfo project={project} openProjectId={null} />
+                  </div>
+                )}
+              </div>
+            ))}
           </div>
-          <div className={styles.boxImageProject}>
-            <img
-              className={styles.projectImage}
-              src={project.image}
-              alt={project.imageAlt}
-            />
-          </div>
-          <div className={styles.visitRepoAndWebsiteButtons}>
-          <div className={styles.styledButton}>
-            <Link href={project.githubUrl}>GitHub</Link>
-          </div>
-          <div className={styles.styledButton}>
-            <Link href={project.url}>Visit Website</Link>
-          </div>
-          </div>
-<div className={styles.technologiesUsed}>
-<TechnologiesUsed technologies={project.technologies} />
-</div>
-         
-          {openProjectId === project._id && (
-            <div className={styles.infoBubble}>
-              <ProjectInfo project={project} openProjectId={null} />
-            </div>
-          )}
         </div>
       ))}
     </div>
   );
 };
 
-// Usando React.memo per evitare renderizzazioni non necessarie
 export default React.memo(ProjectList);
+
+
